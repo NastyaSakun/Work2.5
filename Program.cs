bd163c5be3ecf875993724c394cd6a02f70598e1
Nastya Sakun <sakun_nastya@mail.ru>
2018-09-21 14:03:45 +0300

    efwefew

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] chisla = { 1, 2, 3 };
+            var str = "nastya sakun"; 
+
+            (int, int, int, string) Check(int[] number, string stroka) 
+            {
+                for (var j = 0; j < number.Length - 1; j++) 
+                    if (number[j] > number[j + 1])
+                    {
+                        var temp = number[j];
+                        number[j] = number[j + 1];
+                        number[j + 1] = temp;
+                    }
+
+                var sum = 0;
+                foreach (var i in number) sum += i; 
+
+                var result = (number[0], number[number.Length - 1], sum, str[0].ToString()); 
+                return result;
+            }
+
+            Console.WriteLine("Answer - {0}", Check(chisla, str));
+        }
+    }
+}
